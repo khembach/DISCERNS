8f7c2d73bae2fa32460b24fa196b15b6284ecc5f
Katharina Hembach <3229704+khembach@users.noreply.github.com>
2019-05-21 16:55:34 +0200

    First test for import_novel_sj_reads()

---
+++ tests/testthat/test_import_novel_sj_reads.R
@@ -0,0 +1,28 @@
+context("Import novel SJ reads")
+
+library(exondiscovery)
+
+gtf <- system.file("extdata", "selected.gtf", package = "exondiscovery", 
+                   mustWork = TRUE)
+anno <- prepare_annotation(gtf)
+sj <- system.file("extdata", "selected.SJ.out.tab", 
+                  package = "exondiscovery", mustWork = TRUE)
+bam <- system.file("extdata", "selected.bam", 
+                   package = "exondiscovery", mustWork = TRUE)
+
+sj <- fread(sj)
+colnames(sj) <- c("seqnames", "start", "end", "strand", "motif", "annotated",
+                  "unique", "mutimapping", "maxoverhang")
+sj$strand <- c("*", "+", "-")[sj$strand + 1]
+sj_gr <- GRanges(sj)
+sj_ann <- subsetByOverlaps(sj_gr, anno[["introns"]], type = "equal")
+sj_unann <- sj_gr[!(sj_gr %in% sj_ann)]
+sj_unann <- sj_unann[(start(sj_unann) - 1) %in% end(anno[["exons"]]) |
+                       (end(sj_unann) + 1) %in% start(anno[["exons"]]), ]
+
+reads <- import_novel_sj_reads(bam, sj_unann)
+
+test_that("SJ reads are imported in correct format", {
+  expect_s4_class(reads, "GAlignments")
+  expect_named(mcols(reads), c("qname", "which_label"))
+})
